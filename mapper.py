4a8416e46c57af2492f015406d8bed3894bd36a3
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-07-12 20:52:48 -0500

    improvements

--- mapper.py
+++ mapper.py
@@ -1,6 +1,46 @@
 from pyrosm import OSM, get_data
-import pandas as pd
 import osmnx as ox
+import osmium as osm
+import pandas as pd
+import urllib.request
+import os
+
+class OSMHandler(osm.SimpleHandler):
+    def __init__(self):
+        osm.SimpleHandler.__init__(self)
+        self.osm_data = []
+        
+    def download_data(self,filename,url):
+        if not os.path.exists(filename):
+            filename, headers = urllib.request.urlretrieve(url,filename)   
+
+    def tag_inventory(self, elem, elem_type):
+        for tag in elem.tags:
+            self.osm_data.append([elem_type, 
+                                   elem.id, 
+                                   elem.version,
+                                   elem.visible,
+                                   pd.Timestamp(elem.timestamp),
+                                   elem.uid,
+                                   elem.user,
+                                   elem.changeset,
+                                   len(elem.tags),
+                                   tag.k, 
+                                   tag.v])
+
+    def node(self, n):
+        self.tag_inventory(n, "node")
+
+    def way(self, w):
+        self.tag_inventory(w, "way")
+
+    def relation(self, r):
+        self.tag_inventory(r, "relation")
+
+    def dataframe(self):
+        data_colnames = ['type', 'id', 'version', 'visible', 'ts', 'uid',
+                 'user', 'chgset', 'ntags', 'tagkey', 'tagvalue']
+        return pd.DataFrame(self.osm_data, columns=data_colnames).sort_values(by=['type', 'id', 'ts'])   
 
 class MapParser:
     def __init__(self, data):
@@ -16,7 +56,6 @@ class MapParser:
         self.route=None
         self.Graph=None
 
-        
         
     def get_pois_from_map(self,poi_filter=None):
         """
